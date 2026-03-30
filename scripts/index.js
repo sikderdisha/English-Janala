8c7a303cc197a00ceb5e9e8f12e277ceb6c2cedd
Disha Sikder <sikderdisha715@gmail.com>
2026-03-31 02:39:01 +0600

    add modal card 28.7

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,13 @@ function loadData(){
     .then(res => res.json())
     .then(data => display(data.data)); // ⚠️ important (data.data)
 }
+const removeActive=()=>{
+    const lessonBtn = document.querySelectorAll('.lesson-btn');
+    //remove all active
+    lessonBtn.forEach(btn =>{
+        btn.classList.remove('active');
+    })
+}
 //load word
 //it received the id of the levels
 const loadWord=(id)=>{
@@ -12,8 +19,17 @@ const loadWord=(id)=>{
 
     fetch(url)
     .then(res => res.json())
-    .then(data => displayWord(data.data));
+    .then(data => {
+        //get the id of lesson
+        const clickBtn = document.getElementById(`btn-lesson-${id}`);
+console.log(clickBtn);
+//add active class for btn
+removeActive();
+clickBtn.classList.add('active');
+clickBtn.classList.add('lesson-btn');
+        displayWord(data.data)});
 }
+
 
 //display word
 const displayWord=(words)=>{
@@ -21,6 +37,16 @@ const displayWord=(words)=>{
     const wordContainer =document.getElementById('wordContainer');
 
     wordContainer.innerHTML="";
+
+    //if the card has no data
+    if(words.length == 0){
+        wordContainer.innerHTML=`
+        <div class="col-span-full space-y-4 items-center text-center justify-center flex flex-col">
+        <img src="./assets/alert-error.png" alt="alert icon">
+        <p class="hind-siliguri-light text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="hind-siliguri-light text-4xl font-bold">নেক্সট Lesson এ যান</h2>
+    </div>`
+    }
 //loop
     for(const word of words){
 //create element
@@ -28,14 +54,14 @@ const newCard = document.createElement('div');
 
 newCard.innerHTML=`
 <div class="bg-white rounded space-y-3 px-5 py-15 text-center">
-    <h2 class="text-2xl font-bold">${word.word}</h2>
+    <h2 class="text-2xl font-bold">${word.word ? word.word:"word পাওয়া যায়নি"}</h2>
     <p class="hind-siliguri-ligh font-semibold text-[15px]">Meaning/Pronunciation</p>
     <div>
         <p class="break-words text-2xl font-medium">
-   ${word.meaning}/${word.pronunciation}
+   ${word.meaning ? word.meaning:"Meaning পাওয়া যায়নি"}/${word.pronunciation ? word.pronunciation:"Pronunciation পাওয়া যায়নি"}
 </p>
         <div class="flex justify-between items-center pt-5">
-           <button class="btn bg-[#e8f3fe] hover:bg-[#00BCFF]">
+           <button onclick="my_modal_3.showModal()" class="btn bg-[#e8f3fe] hover:bg-[#00BCFF]">
     <i class="fa-solid fa-circle-info"></i>
 </button>
 
@@ -62,7 +88,7 @@ const display = (lessons) => {
 
         // content add
         newContainer.innerHTML = `
-           <button onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-info"><i class="fa-brands fa-leanpub"></i> Lesson-${lesson.level_no}</button>
+           <button id="btn-lesson-${lesson.level_no}" onclick="loadWord(${lesson.level_no})" class="btn btn-outline btn-info"><i class="fa-brands fa-leanpub"></i> Lesson-${lesson.level_no}</button>
         `;
 
         // append
